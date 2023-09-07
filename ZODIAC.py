693c501bbef3e4d13c7b2079f3e115427d882862
Zodiac1731 <144370563+Zodiac1731@users.noreply.github.com>
2023-09-07 19:56:48 +0330

    Create ZODIAC.py

---
+++ ZODIAC.py
@@ -0,0 +1,10 @@
+- 👋 Hi, I’m @Zodiac1731
+- 👀 I’m interested in ...python
+- 🌱 I’m currently learning ...python
+- 💞️ I’m looking to collaborate on ..python.
+- 📫 How to reach me ...
+
+<!---
+Zodiac1731/Zodiac1731 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
